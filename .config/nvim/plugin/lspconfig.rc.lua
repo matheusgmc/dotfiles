fe2d3b674fce5d277539bfa97fb2cc3013e34cc0
matheusgmc <sdmastergames@hotmail.com>
2022-09-06 09:55:01 -0400

    add jsonls

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -2,6 +2,8 @@ local status, lspconfig = pcall(require,"lspconfig")
 
 if (not status) then return end
 
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -28,12 +30,25 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   --filetypes = {"typescript", "typescriptreact", "typescript.tsx"},
   capabilities = capabilities,
   root_dir = function() return vim.loop.cwd() end
+}
+
+lspconfig.jsonls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    jsonls = {
+      files = {
+        association = {"*.json","jsonc"}
+      }
+    }
+  }
 }
 
 lspconfig.tailwindcss.setup {}
@@ -61,3 +76,12 @@ lspconfig.rust_analyzer.setup {
   capabilities = capabilities,
   
 }
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  update_in_insert = false,
+  virtual_text = { spacing = 4, prefix = "●" },
+  severity_sort = true,
+}
+)
