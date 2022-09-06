96a603911214f32a665813c3bf04b5765d5eda47
matheusgmc <sdmastergames@hotmail.com>
2022-09-06 09:53:30 -0400

    maps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,10 +2,21 @@ local opts = {noremap = true, silent = true}
 
 local keymap = vim.api.nvim_set_keymap
 
+
 --- Normal
 -- Move text up and down
 keymap("n","<A-j>",":m .+1<CR>==",opts)
 keymap("n","<A-k>",":m .-2<CR>==",opts)
+
+keymap("n","<C-h>","b",opts)
+keymap("n","<C-l>","e",opts)
+
+-- Increment/decrement
+keymap("n","+","<C-a>",opts)
+keymap("n","-","<C-x>",opts)
+
+-- Select all
+keymap("n","<C-a>",'gg<S-v>G',opts)
 
 --- Visual
 -- Move text up and down
